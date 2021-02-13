bf2a66a490c8892c05006ad4273bddfa950a20db
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-12 19:49:17 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import { Provider } from 'react-redux'
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <RestaurantInput />
-        <Restaurants />
+            <RestaurantInput />
+            <Restaurants />
       </div>
     );
   }
