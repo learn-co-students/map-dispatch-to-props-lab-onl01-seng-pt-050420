8d2c1599d22be25358bafd04b663f7f5d93ca24f
ParishBen <ParishBen13@gmail.com>
2021-02-13 11:38:56 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -58,5 +58,7 @@ const mapDispatchToProps = dispatch => {
 // Alternative form:
 
 // export default connect(null, { addRestaurant })(RestaurantInput);
+
+
 //connect this component by wrapping RestaurantInput below
 export default connect(null, mapDispatchToProps)(RestaurantInput)
